0bc36a098ca2a32a8f8c5f29ec26020796765bbd
unknown <abdelhamed.muhammed19@gmail.com>
2025-09-25 19:10:55 +0300

    Access Control

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
